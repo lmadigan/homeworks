d5504f8a371519b93fce3947e95a8b2f4bdb9ea6
lmadigan <laurenmadigan@Laurens-MacBook-Pro.local>
2016-12-25 19:28:32 -0800

    adding places to a list

--- w6d4/vanilla-dom/script.js
+++ w6d4/vanilla-dom/script.js
@@ -18,20 +18,37 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // adding SF places as list items
 
-  const listContainerEl = document.getElementById("list-form");
+  // const listContainerEl = document.getElementById("list-form");
+  //
+  // listContainerEl.addEventListener("submit", event => {
+  //   event.preventDefault();
+  //
+  //   const newFavoriteInputEl = document.getElementByClassName("favorite-submit");
+  //   const newFavorite = newFavoriteInputEl.value;
+  //   newFavoriteInputEl.value = "";
+  //
+  //   const places = document.getElementById("sf-places");
+  //   const li = document.createElement("li");
+  //   li.textContent = newFavorite;
+  //   places.appendChild(li);
+  // });
 
-  listContainerEl.addEventListener("submit", event => {
-    event.preventDefault();
+  const handleFavoriteSubmit = (e) => {
+    e.preventDefault();
 
-    const newFavoriteInputEl = document.getElementByClassName("favorite-submit");
-    const newFavorite = newFavoriteInputEl.value;
-    newFavoriteInputEl.value = "";
+    const favoriteInput = document.querySelector(".favorite-input");
+    const favorite = favoriteInput.value;
+    favoriteInput.value = "";
 
-    const places = document.getElementById("sf-places");
-    const li = document.createElement("li");
-    li.textContent = newFavorite;
-    places.appendChild(li);
-  });
+    const newListLi = document.createElement("li");
+    newListLi.textContent = favorite;
+
+    const favoritesList = document.getElementById("sf-places");
+    favoritesList.appendChild(newListLi);
+
+  };
+  const listSubmitButton = document.querySelector(".favorite-submit");
+  listSubmitButton.addEventListener("click", handleFavoriteSubmit);
 
   // --- your code here!
 
